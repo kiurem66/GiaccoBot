d0e3a3fe69dc67bd18f15565417b84ed3d364f98
Matteo Mirasolo <kiurem66@gmail.com>
2022-07-29 00:07:07 +0200

    bot finished

--- Giacco.py
+++ Giacco.py
@@ -1,14 +1,18 @@
 import telebot
-import pickle
 
-admins = [640632571]
+admins = [640632571, 156707897]
 users = []
 
 reg_err = "Mi di spiace non sei un membro della gilda ghe ghe ghe\n Prova ad usere /register"
 not_adm = "Mi dispiace ma accetto ordini solo da Bridge, Thalar e Aiden, Ghe Ghe"
-group_id = 0
+group_id = -793240876
 
 xpnextlvl = [0, 0, 0, 0, 0, 7500, 15000, 17800, 20000, 24500]
+
+
+
+token = open("token.txt", "r").readline()
+bot = telebot.TeleBot(token, False)
 
 class user:
     def __init__(self, nome, id, level):
@@ -32,7 +36,10 @@ class user:
             self.xp -= xpnextlvl[self.level]
             self.level += 1
     
-    
+def write_users():
+    f = open("users","wb")
+    pickle.dump(users, f)
+    f.close()
 
 def extract_arg(arg):
     command_length = len(arg.split()[0])
@@ -67,13 +74,16 @@ def myxp(message):
 @bot.message_handler(commands=["giveall"])
 def giveall(message):
     if(isAdmin(message.from_user.id)):
-        try
+        try:
             numXp = int(extract_arg(message.text))
             for u in users:
                 if u.addXp(numXp):
                     bot.reply_to(message, u.nome+ " ha livellato, ora è migliore ma non quanto un Goblin!")
+            write_users()    
         except Exception:
             bot.reply_to(message, "Comando errato, ghe")
+    else:
+        bot.reply_to(message, not_adm)
 
 
 bot.message_handler(commands={"saytogroup"})
@@ -84,6 +94,8 @@ def say(message):
             bot.send_message(group_id, to_say)
         except Exception:
             bot.reply_to(message, "Comando errato, ghe")
+    else:
+        bot.reply_to(message, not_adm)
 
 
 bot.message_handler(commands={"levelup"})
@@ -99,21 +111,25 @@ def levelup(message):
         bot.reply_to(message, "Hai provasto a fregarmi! Tu non puoi livellare!")
         return
     user.levelup()
+    write_users()
 
 
 bot.message_handler(commands={"give", "givepx", "givexp"})
 def give(message):
     if(isAdmin(message.from_user.id)):
-        try
+        try:
             numXp = int(extract_arg(message.text).split[1])
             name = extract_arg(message.text).split[0]
             for user in users:
                 if user.name == name:
                     user.addXp(numXp)
+                    write_users()
                     return
             bot.reply_to(message, "Non so di chi parli, ghe")
         except Exception:
             bot.reply_to(message, "Comando errato, ghe")
+    else:
+        bot.reply_to(message, not_adm)
 
 bot.message_handler(commands={"register"})
 def register(message):
@@ -124,6 +140,41 @@ def register(message):
         name = extract_arg(message.text).split[0]
         lvl = int(extract_arg(message.text).split[1])
         users.append(User(name, id, level))
+        write_users()
         bot.reply_to(message, "Perfetto, ora mi ricorderò di te, ghe ghe ghe")
     except Exception:
             bot.reply_to(message, "Comando errato, ghe")
+
+@bot.message_handler(commands=["delete"])
+def deluser(message):
+    u = get_user(message.from_user.id)
+    if u != None:
+        try:
+            response = extract_arg(message.text)
+            if response == "YES":
+                users.remove(u)
+                write_users()
+                bot.reply_to(message, "Operazione completata... Chi cazzo sei?")
+            else:
+                bot.reply_to(message, user_error)
+                print(response)
+        except:
+            bot.reply_to(message, "ATTENZIONE, stai elminando il tuo profilo e i tuoi px\n se sei sicuro ripeti il comando con l'aggiunta di YES in questo modo.\n /deluser YES")            
+    else:
+        bot.reply_to(message, "Mi dispiace, non ho la minima idea di chi tu sia, ghe ghe!")
+
+
+
+
+#caricamento utenti
+try:
+    f = open("users","rb")
+    users = pickle.load(f)
+    f.close()
+except:
+    users = []
+
+while True:
+    bot.delete_webhook()
+    bot.polling(none_stop=True)
+    print("bot reboot")
